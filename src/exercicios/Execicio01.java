20649d52185ebedded73575f9bf22463f6634ee3
itajacy TI02 <itajacy@bsd.com.br>
2026-01-29 16:42:38 -0300

    exercicio feito

---
+++ src/exercicios/Execicio01.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Execicio01 {
+    static void main() {
+        String nomeDaMissao = "Apolo 12";
+        int numeroDeTripulantes = 3;
+        long destinoEmKm = 150000000000L;
+        boolean missaoAtiva = true;
+        double nivelDeOxigenio = 98.5;
+
+        System.out.println("--- DADOS DA MISSÃO ---");
+        System.out.println("Nome: " + nomeDaMissao);
+        System.out.println("Tripulação: " + numeroDeTripulantes);
+        System.out.println("Distância: " + destinoEmKm + " km");
+        System.out.println("Status Ativo: " + missaoAtiva);
+        System.out.println("Nível Oxigênio: " + nivelDeOxigenio +"%");
+
+
+    }
+}
