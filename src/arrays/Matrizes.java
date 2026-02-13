e7e33b12714fde8ee213a4104475da519f3efb47
itajacy TI02 <itajacy@bsd.com.br>
2026-02-13 16:06:37 -0300

    matrizes

---
+++ src/arrays/Matrizes.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Matrizes {
+    static void main() {
+        int[][] matrizSimples = new int[2][2];
+        matrizSimples[0][0] = 1;
+        matrizSimples[0][1] = 2;
+        matrizSimples[1][0] = 3;
+        matrizSimples[1][1] = 4;
+
+        System.out.println(matrizSimples[0][0]);
+        System.out.println(matrizSimples[0][1]);
+        System.out.println(matrizSimples[1][0]);
+        System.out.println(matrizSimples[1][1]);
+
+        int[][] matrizEmLinha = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+                {10,11,12}
+        };
+
+        System.out.println(Arrays.deepToString(matrizEmLinha));
+
+        System.out.println(matrizEmLinha[2][1]);
+
+    }
+}
