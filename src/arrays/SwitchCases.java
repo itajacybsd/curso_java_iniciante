b23fb778c77d24d0b4ac54702d880177ca5e7583
ita Dev <itajacy@gmail.com>
2026-02-14 11:34:39 -0300

    switch case

---
+++ src/arrays/SwitchCases.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class SwitchCases {
+    static void main() {
+        int diaSemana = 7;
+        switch (diaSemana) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Dia inválido");
+
+        }
+    }
+}
