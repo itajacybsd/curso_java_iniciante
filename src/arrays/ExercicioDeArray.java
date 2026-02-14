9b5d73175b2750086760beb53c18027f8721648e
ita Dev <itajacy@gmail.com>
2026-02-14 08:30:32 -0300

    Exercicio de Arrays, com o desafio proposto

--- src/arrays/ExercicioDeArray.java
+++ src/arrays/ExercicioDeArray.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class ExercicioDeArray {
     static void main() {
-        double[] notas = {7.5, 8.0,6.5,9.0,7.0};
+        double[] notas = {7.5, 8.0,6.5,9.0,7.0, 3.5, 8.5,5.5,9.5,4.0};
         double soma = 0.0;
         double media = 0.0;
         double maior = 0.0;
         double menor = notas[0];
+        int notasAcimaDaMedia = 0;
         for (int i = 0; i<notas.length;i++) {
             System.out.println("Nota " + (i+1) + ": " + notas[i]);
             if(notas[i]>maior) {
@@ -20,11 +21,17 @@ public class ExercicioDeArray {
             soma += notas[i];
             media = soma /  notas.length;
         }
+        for (int i = 0; i<notas.length;i++) {
+            if(notas[i]>media) {
+                notasAcimaDaMedia++;
+            }
+        }
         System.out.println("");
         System.out.println("Soma: " + soma);
         System.out.println("Média: " + media);
         System.out.println("Maior: " + maior);
         System.out.println("Menor: " + menor);
+        System.out.println("Notas Acima da media: " + notasAcimaDaMedia);
 
     }
 }
