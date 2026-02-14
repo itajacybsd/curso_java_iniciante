901badb6bdb2b0c41b0bfdbae348892cb5b1e9ff
ita Dev <itajacy@gmail.com>
2026-02-14 13:32:02 -0300

    lacos de repeticao 1

---
+++ src/lacosRepeticoes/For.java
@@ -0,0 +1,22 @@
+package lacosRepeticoes;
+
+public class For {
+    static void main() {
+        for (int i = 0; i <=5; i++) {
+            System.out.println(i);
+        }
+            System.out.println("---------------------");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+            System.out.println("---------------------");
+
+        int[] numeros = {10,20,30,40};
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+
+        }
+            System.out.println("---------------------");
+    }
+}
---
+++ src/lacosRepeticoes/While.java
@@ -0,0 +1,22 @@
+package lacosRepeticoes;
+
+
+
+public class While {
+    static void main() {
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("Contador: " + contador  );
+            contador++;
+
+
+        }
+
+        int senha = 0;
+        while (senha != 1234) {
+            System.out.println("Digite a Senha: ");
+            senha = Integer.parseInt(IO.readln());
+
+        }
+    }
+}
